fc604122380c553f8a5a472e2e20d2fa4eb4dfd3
Denis <web@dgladkov.ru>
2020-06-22 12:03:27 +0300

    Update breadcrumbs.php

--- wp-content/themes/jetcamp-child/breadcrumbs.php
+++ wp-content/themes/jetcamp-child/breadcrumbs.php
@@ -19,7 +19,15 @@ if ( $breadcrumbs || $page_title ) :
 		wp_enqueue_script( 'skrollr' );
 	}
 	?>
-	
+	<?php if ( 'boxed' != porto_get_wrapper_type() && 'boxed' == $porto_settings['breadcrumbs-wrapper'] ) : ?>
+		<div id="breadcrumbs-boxed">
+	<?php endif; ?>
+	<section class="page-top<?php echo 'wide' == $porto_settings['breadcrumbs-wrapper'] ? ' wide' : ''; ?> page-header-<?php echo esc_attr( $page_header_type ); ?><?php echo isset( $porto_settings['breadcrumbs-css-class'] ) && $porto_settings['breadcrumbs-css-class'] ? ' ' . esc_attr( $porto_settings['breadcrumbs-css-class'] ) : ''; ?>"<?php echo ! $porto_settings['breadcrumbs-parallax'] ? '' : ' data-plugin-parallax data-plugin-options="{&quot;speed&quot;: ' . esc_attr( $porto_settings['breadcrumbs-parallax-speed'] ) . '}"'; ?>>
+		<?php get_template_part( 'page_header/page_header_' . $page_header_type ); ?>
+	</section>
+	<?php if ( 'boxed' != porto_get_wrapper_type() && 'boxed' == $porto_settings['breadcrumbs-wrapper'] ) : ?>
+		</div>
+	<?php endif; ?>
 <?php elseif ( is_customize_preview() ) : ?>
 	<section class="page-top d-none"></section>
 <?php endif; ?>
