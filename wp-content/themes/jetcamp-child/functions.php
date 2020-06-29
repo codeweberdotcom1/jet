2dd2588fa31bf0f4709c2d7edbaf9f56329f2ba7
Denis <web@dgladkov.ru>
2020-06-30 00:24:58 +0300

    Update functions.php

--- wp-content/themes/jetcamp-child/functions.php
+++ wp-content/themes/jetcamp-child/functions.php
@@ -323,7 +323,7 @@ function woocommerce_my_new_title(){
     echo '<div class="product_meta mt-1 mb-4">
 
     <div class="phone-product"><span class="phone-product-w">Телефон: </span>
-<i class="fa fa-phone" aria-hidden="true"></i>+7(926)-170-13-33</a></span></div>
+<a href="tel:+79261701333"<i class="fa fa-phone" aria-hidden="true"></i>+7(926)170-13-33</a></span></div>
 
     <div class="phone-product"><span class="phone-product-w">E-Mail: </span><span><a href="mailto:connect@jetcamp.ru"><i class="fa fa-envelope" aria-hidden="true"></i> connect@jetcamp.ru</a></span></div>
     </div>';
