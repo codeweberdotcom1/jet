0e5a2588459cba1b5f53eaa53892e16d2406c53e
Denis <web@dgladkov.ru>
2020-06-29 23:48:12 +0300

    Update functions.php

--- wp-content/themes/jetcamp-child/functions.php
+++ wp-content/themes/jetcamp-child/functions.php
@@ -320,14 +320,5 @@ add_filter('woocs_raw_woocommerce_price', function($price) {
 
 add_action( 'woocommerce_single_product_summary', 'woocommerce_my_new_title', 30 );
 function woocommerce_my_new_title(){
-    echo '<span class="">Телефон: <span class=""><aside id="follow-us-widget-3" class="widget follow-us follow-us-zw d-flex align-items-center my-3"></span>
-
-    <span>Телефон:</span><span><a href="tel:+79261701333"><i class="fa fa-phone" aria-hidden="true"></i>+7(926)-170-13-33</a>
-
-<span class="share-links"><a class="share-telegram" title="" href="https://t.me/JetskiCamp" target="_blank" rel="nofollow noopener noreferrer" data-toggle="tooltip" data-placement="bottom" data-original-title="Telegram">Telegram</a>
-
-<a class="share-whatsapp" style="display: none;" title="" href="https://api.whatsapp.com/send?phone=79261701333" target="_blank" rel="nofollow noopener noreferrer" data-toggle="tooltip" data-placement="bottom" data-original-title="WhatsApp">WhatsApp</a>
-</span>
-<span class="d-flex text_phone pl-2"><i class="Simple-Line-Icons-phone pr-2"></i><a href="tel:+79261701333">+7(926)-170-13-33</a></span>
-</aside></span></span>';
+    echo '<span>Телефон:</span><span><span><a href="tel:+79261701333"><i class="fa fa-phone" aria-hidden="true"></i>+7(926)-170-13-33</a></span>';
 }
