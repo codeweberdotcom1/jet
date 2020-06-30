ab3984aef8c006ed8d8d73c7ca1c82dbe521c441
Denis <web@dgladkov.ru>
2020-07-01 00:12:41 +0300

    Update functions.php

--- wp-content/themes/jetcamp-child/functions.php
+++ wp-content/themes/jetcamp-child/functions.php
@@ -350,7 +350,7 @@ function ggstyle_menu_item_count( $output, $item, $depth, $args ) {
 }
 add_action( 'walker_nav_menu_start_el', 'ggstyle_menu_item_count', 10, 4 );
 
-add_action( 'woocommerce_before_shop_loop_item_title', 'custom_before_title' );
+add_action( 'woocommerce_after_shop_loop_item_title', 'custom_before_title' );
 function custom_before_title() {
 
     global $product;
